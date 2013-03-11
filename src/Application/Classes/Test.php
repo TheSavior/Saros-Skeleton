b24b2a64e2df7ae51d4d3a2ca024f197acefa0e5
TheSavior <github@eli-white.com>
2013-03-11 01:26:15 -0700

    Adding default tests and tie in to travis-ci

---
+++ src/Application/Classes/Test.php
@@ -0,0 +1,16 @@
+<?php
+namespace Application\Classes;
+
+/**
+* This class is to show that the tests can access the Application classes.
+*/
+class Test {
+    function __construct() {
+
+    }
+
+    function getTestValue() {
+        return 42;
+    }
+}
+?>
